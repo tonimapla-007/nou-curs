6dce35338b20010cd037f33df74e3200add2f38f
Toni <173633916+tonimapla-007@users.noreply.github.com>
2024-08-04 22:16:23 +0200

    funciones

---
+++ tipos/cntro-flujo/funciones/05-alcance.py
@@ -0,0 +1,15 @@
+saludo = "hola global "
+
+
+def saludar():
+    saludo = "Hola mundo"
+
+
+def saludaChanchito():
+    saludo = "Hola Chanchito"
+    print(saludo)
+
+
+saludar()
+# saludaChanchito()
+# saludar()
