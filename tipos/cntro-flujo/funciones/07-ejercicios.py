364cc491d759fa3101906108f6294a4052c845d1
Toni <173633916+tonimapla-007@users.noreply.github.com>
2024-08-05 22:18:40 +0200

    funciones, control de flujo

---
+++ tipos/cntro-flujo/funciones/07-ejercicios.py
@@ -0,0 +1,10 @@
+def es_palindromo(texto):
+
+
+print("Abba", es_palindromo("Abba"))
+print("Reconocer", es_palindromo("Reconocer"))
+print("Amo la paloma", es_palindromo("Amo la paloma"))
+print("hola mundo", es_palindromo("hola mundo"))
+
+
+es_palindromo(texto)
